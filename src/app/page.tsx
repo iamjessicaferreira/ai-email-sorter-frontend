1bf8fec199836cdcb5f0fe0794b9b4d5b598cf35
Jessica Ferreira <jessicaferreira@Air-de-Jessica.lan>
2025-07-21 13:01:58 +0200

    fix ws url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -176,7 +176,7 @@ const addCategory = async () => {
   };
 
   const connectWebSocket = () => {
-    const sock = new WebSocket("ws://localhost:8000/ws/emails/");
+    const sock = new WebSocket(`ws://${backendUrl}/ws/emails/`);
     socketRef.current = sock;
     sock.onopen = () => {};
     sock.onmessage = (evt) => {
